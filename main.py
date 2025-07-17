07c9c265bb13940d8ca97b3329a55fead3bc496b
ZayanMolla <zayanmolla2013@gmail.com>
2025-07-17 10:37:50 -0400

    Finished Code

---
+++ main.py
@@ -0,0 +1,17 @@
+from tkinter import *
+window = Tk()
+window.geometry("300x300")
+window.title("Cookie Clicker")
+window.configure(bg='b79448')
+number = 0
+def ClickBtn():
+    global number
+    number += 1
+    ShowInfo["text"] = "You Clicked " + str(number) + "times."
+ClickingImage = PhotoImage(file="cookie.png")
+ClickBtn = Button(window, image=ClickingImage, command=ClickBtn, bg='fab25f',
+fg="blue", font=("Arial", 22), command=ClickBtn)
+ShowInfo = Label(window, text="message", font=("Arial", 20),fg = "purple")
+ClickBtn.pack()
+ShowInfo.pack()
+window.mainloop()
