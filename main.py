b996763662652b9416ef98a996ef8fa29b1865c2
ZayanMolla <zayanmolla2013@gmail.com>
2025-07-17 11:15:22 -0400

    Adding Multiplier

--- main.py
+++ main.py
@@ -3,17 +3,20 @@ from tkinter import *
 window = Tk()
 window.geometry("300x300")
 window.title("Cookie Clicker")
-window.configure(bg='yellow')
+window.configure(bg='darkblue')
 number = 0
 
 def ClickBtn():
     global number
-    number += 1
+    number += multiplier
     ShowInfo["text"] = "Your Score Is " + str(number)
+    Score = str(number)
+    if number => 10:
+        multiplier = 2
 
 ClickingButton = Button(window, text="Click Me!", command=ClickBtn, bg='white',
 fg="black", font=("Arial", 22))
-ShowInfo = Label(window, text="Your Score Is 0", font=("Arial", 20),fg = "purple")
+ShowInfo = Label(window, text="Your Score Is 0", font=("Arial", 20),fg = "black")
 ClickingButton.pack()
 ShowInfo.pack()
 window.mainloop()
